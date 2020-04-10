6f1f077c05340f54d020361b88da0896858f6cd4
redwell <noise1129@gmail.com>
2020-04-10 16:36:38 +0900

    gain

--- WebAudioAPI/audio.js
+++ WebAudioAPI/audio.js
@@ -6,6 +6,12 @@ function initiate(){
   button.addEventListener("click",()=>{
     play(mybuffer);
   },false)
+  var button2 = document.getElementById("button2");
+  button2.addEventListener("click",()=>{
+    play2(mybuffer);
+  },false)
+
+
 
   context = new AudioContext();
 
@@ -29,6 +35,16 @@ function play(mybuffer){
   sourceNode.buffer = mybuffer;
   var volumeNode = context.createGain();
   volumeNode.gain.value = 0.2;
+  sourceNode.connect(volumeNode);
+  volumeNode.connect(context.destination);
+  sourceNode.start(0);
+}
+
+function play2(mybuffer){
+  var sourceNode = context.createBufferSource();
+  sourceNode.buffer = mybuffer;
+  var volumeNode = context.createGain();
+  volumeNode.gain.value = 1.0;
   sourceNode.connect(volumeNode);
   volumeNode.connect(context.destination);
   sourceNode.start(0);
